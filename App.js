cda247cb2abe1866ebf990414d9f2852df67ed68
SamuelSacristan <s.sacristan64@hotmail.com>
2025-01-02 17:17:22 +0100

    testing

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './navigation/AppNavigator'; // assure-toi que le chemin est correct
-
+//aa//
 export default function App() {
   return (
     <NavigationContainer>
